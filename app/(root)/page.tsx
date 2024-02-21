8e87d8b5c25c2a99f2ece7f556245e89366d899a
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-21 09:44:21 +0530

    Added clerk auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>HomePage</p>
+
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  )
+}
+
+export default HomePage
